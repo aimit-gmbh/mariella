a530ef4321e4e5b541ceca6ec13722878d5671bf
ssadat-guscheh-aimit <sascha.sadat-guscheh@scinteco.com>
2024-07-11 14:37:57 +0200

    bump dependencies and change .gitignore

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,6 +10,6 @@ repositories {
 dependencies {
     implementation(kotlin("gradle-plugin", "2.0.0"))
     implementation("com.github.ben-manes:gradle-versions-plugin:0.51.0")
-    implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.7.6")
-    implementation("com.vanniktech:gradle-maven-publish-plugin:0.28.0")
+    implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.8.2")
+    implementation("com.vanniktech:gradle-maven-publish-plugin:0.29.0")
 }
--- buildSrc/src/main/kotlin/at/aimit/mariella/Versions.kt
+++ buildSrc/src/main/kotlin/at/aimit/mariella/Versions.kt
@@ -3,7 +3,7 @@ package at.aimit.mariella
 const val vertxVersion = "4.5.8"
 const val logbackVersion = "1.5.6"
 const val postgresVersion = "42.7.3"
-const val flywayVersion = "10.13.0"
+const val flywayVersion = "10.15.2"
 const val h2Version = "2.2.220"
 const val hikariVersion = "5.1.0"
 const val scramClientVersion = "2.1"

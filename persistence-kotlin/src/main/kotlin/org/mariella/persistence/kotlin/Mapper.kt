7bb6cfeadea2d01b81cc0a6a84dca6f28d791909
ssadat-guscheh-aimit <sascha.sadat-guscheh@scinteco.com>
2024-07-12 15:06:33 +0200

    add support for value classes for the mapper

--- persistence-kotlin/src/main/kotlin/org/mariella/persistence/kotlin/Mapper.kt
+++ persistence-kotlin/src/main/kotlin/org/mariella/persistence/kotlin/Mapper.kt
@@ -13,40 +13,39 @@ import java.time.ZoneId
 import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
 import kotlin.reflect.KParameter
+import kotlin.reflect.full.memberProperties
 import kotlin.reflect.full.primaryConstructor
 
-class Mapper internal constructor(
-    private val sqlClient: SqlClient,
-    private val converterRegistry: ImmutableConverterRegistry
-) {
+class Mapper internal constructor(private val sqlClient: SqlClient, private val converterRegistry: ImmutableConverterRegistry) {
 
-    private class ParameterAndConvert(
-        val parameter: KParameter,
-        val converter: org.mariella.persistence.database.Converter<*>
-    )
+    private data class ConverterAndValueClass(val converter: org.mariella.persistence.database.Converter<*>, val valueClass: KClass<*>?)
+
+    private data class ParameterAndConvert(val parameter: KParameter, val converterWithClass: ConverterAndValueClass)
+
+    private fun KClass<*>.getValueClassSingleType() = (primaryConstructor!!.parameters.single().type.classifier as KClass<*>).javaObjectType
+
+    private fun KClass<*>.getValueClassInstance(data: Any) = primaryConstructor!!.call(data)
 
     private fun <T : Any> RowSet<Row>.mapRowsToObjects(
-        kClass: KClass<T>,
-        converterRegistry: ImmutableConverterRegistry,
-        primaryConstructor: KFunction<Any>
+        kClass: KClass<T>, converterRegistry: ImmutableConverterRegistry, primaryConstructor: KFunction<Any>
     ): List<T> {
         val converters = columnDescriptors().map { columnDescriptor ->
             val parameter = primaryConstructor.parameters.singleOrNull {
                 it.name!!.lowercase() == columnDescriptor.name().lowercase()
-            } ?: error(
-                "no constructor parameter for class ${kClass.simpleName} with name ${
-                    columnDescriptor.name().lowercase()
-                }"
-            )
+            } ?: error("no constructor parameter for class ${kClass.simpleName} with name ${columnDescriptor.name().lowercase()}")
 
             val field = kClass.javaObjectType.declaredFields.single { parameter.name == it.name }
             val annotation = field.getDeclaredAnnotation(Converter::class.java)
 
             val converter = if (annotation == null) {
-                val clazz = (parameter.type.classifier as KClass<*>).javaObjectType
-                converterRegistry.getConverterForColumn(clazz, columnDescriptor.jdbcType())
+                val targetClass = parameter.type.classifier as KClass<*>
+                if (targetClass.isValue) {
+                    ConverterAndValueClass(converterRegistry.getConverterForColumn(targetClass.getValueClassSingleType(), columnDescriptor.jdbcType()), targetClass)
+                } else {
+                    ConverterAndValueClass(converterRegistry.getConverterForColumn(targetClass.javaObjectType, columnDescriptor.jdbcType()), null)
+                }
             } else {
-                converterRegistry.getNamedConverter(annotation.name)
+                ConverterAndValueClass(converterRegistry.getNamedConverter(annotation.name), null)
             }
             ParameterAndConvert(parameter, converter)
         }
@@ -55,9 +54,14 @@ class Mapper internal constructor(
             val paramValues = mutableMapOf<KParameter, Any?>()
 
             converters.forEachIndexed { index, parameterAndConvert ->
-                val value = parameterAndConvert.converter.getObject(resultRow, index + 1)
-                if (!(parameterAndConvert.parameter.isOptional && value == null))
-                    paramValues[parameterAndConvert.parameter] = value
+                val value = parameterAndConvert.converterWithClass.converter.getObject(resultRow, index + 1)
+                if (!(parameterAndConvert.parameter.isOptional && value == null)) {
+                    if (parameterAndConvert.converterWithClass.valueClass != null) {
+                        paramValues[parameterAndConvert.parameter] = parameterAndConvert.converterWithClass.valueClass.getValueClassInstance(value)
+                    } else {
+                        paramValues[parameterAndConvert.parameter] = value
+                    }
+                }
             }
             @Suppress("UNCHECKED_CAST")
             primaryConstructor.callBy(paramValues) as T
@@ -80,8 +84,7 @@ class Mapper internal constructor(
         val primaryConstructor = clazz.primaryConstructor!!
 
         primaryConstructor.parameters.forEach {
-            if (it.isOptional && it.type.isMarkedNullable)
-                error("mapping a nullable type with default value does not make sense")
+            if (it.isOptional && it.type.isMarkedNullable) error("mapping a nullable type with default value does not make sense")
         }
         return primaryConstructor
     }
@@ -91,19 +94,13 @@ class Mapper internal constructor(
         return flow {
             val ex = RuntimeException("cursor failed -> $sql")
             val cursor = try {
-                @Suppress("SqlSourceToSinkFlow")
-                (sqlClient as SqlConnection).prepare(sql).coAwait().cursor(getTuple(params))
+                @Suppress("SqlSourceToSinkFlow") (sqlClient as SqlConnection).prepare(sql).coAwait().cursor(getTuple(params))
             } catch (e: Throwable) {
                 throw ex.initCause(e)
             }
             try {
-                cursor.read(batchSize).coAwait().mapRowsToObjects(clazz, converterRegistry, primaryConstructor).forEach {
-                    emit(it)
-                }
-                while (cursor.hasMore())
-                    cursor.read(batchSize).coAwait().mapRowsToObjects(clazz, converterRegistry, primaryConstructor).forEach {
-                        emit(it)
-                    }
+                cursor.read(batchSize).coAwait().mapRowsToObjects(clazz, converterRegistry, primaryConstructor).forEach { emit(it) }
+                while (cursor.hasMore()) cursor.read(batchSize).coAwait().mapRowsToObjects(clazz, converterRegistry, primaryConstructor).forEach { emit(it) }
             } catch (e: Throwable) {
                 throw ex.initCause(e)
             } finally {
@@ -112,21 +109,13 @@ class Mapper internal constructor(
         }
     }
 
-    suspend inline fun <reified T : Any> select(sql: String, vararg params: Any?): List<T> {
-        return select(sql, T::class, params = params)
-    }
+    suspend inline fun <reified T : Any> select(sql: String, vararg params: Any?): List<T> = select(sql, T::class, params = params)
 
-    inline fun <reified T : Any> cursor(sql: String, vararg params: Any?, batchSize: Int = 1000): Flow<T> {
-        return cursor(sql, T::class, params = params, batchSize)
-    }
+    inline fun <reified T : Any> cursor(sql: String, vararg params: Any?, batchSize: Int = 1000): Flow<T> = cursor(sql, T::class, params = params, batchSize)
 
-    suspend inline fun <reified T : Any> selectOne(sql: String, vararg params: Any?): T? {
-        return select<T>(sql, params = params).singleOrNull()
-    }
+    suspend inline fun <reified T : Any> selectOne(sql: String, vararg params: Any?): T? = select<T>(sql, params = params).singleOrNull()
 
-    suspend inline fun <reified T> selectPrimitive(sql: String, vararg params: Any?): List<T> {
-        return selectPrimitive(sql, T::class, params = params)
-    }
+    suspend inline fun <reified T> selectPrimitive(sql: String, vararg params: Any?): List<T> = selectPrimitive(sql, T::class, params = params)
 
     suspend fun <T> selectPrimitive(sql: String, clazz: KClass<*>, vararg params: Any?): List<T> {
         val ex = RuntimeException("query failed -> $sql")
@@ -138,13 +127,12 @@ class Mapper internal constructor(
                 error("expected a single result column for select $sql")
             }
             val converter = converterRegistry.getConverterForColumn(
-                clazz.java,
-                result.columnDescriptors().single().jdbcType()
+                if (clazz.isValue) clazz.getValueClassSingleType() else clazz.java, result.columnDescriptors().single().jdbcType()
             )
             return result.map {
                 val row = VertxResultRow(it)
-                @Suppress("UNCHECKED_CAST")
-                converter.getObject(row, 1) as T
+                @Suppress("UNCHECKED_CAST") if (clazz.isValue) clazz.getValueClassInstance(converter.getObject(row, 1)) as T
+                else converter.getObject(row, 1) as T
             }
         } catch (e: Throwable) {
             throw ex.initCause(e)
@@ -158,8 +146,7 @@ class Mapper internal constructor(
     suspend fun executeBatch(sql: String, params: List<List<Any?>>, batchSize: Int = 10000): List<RowSet<Row>> {
         val ex = RuntimeException("batch operation failed -> $sql")
         try {
-            @Suppress("SqlSourceToSinkFlow")
-            val st = sqlClient.preparedQuery(sql)
+            @Suppress("SqlSourceToSinkFlow") val st = sqlClient.preparedQuery(sql)
             return params.chunked(batchSize).map { sublist ->
                 st.executeBatch(sublist.map { getTuple(it.toTypedArray()) }).coAwait()
             }
@@ -168,27 +155,23 @@ class Mapper internal constructor(
         }
     }
 
-    private suspend fun runQuery(
-        sql: String,
-        params: Array<out Any?>
-    ) = sqlClient.preparedQuery(sql).execute(getTuple(params)).coAwait()
+    private suspend fun runQuery(sql: String, params: Array<out Any?>) = sqlClient.preparedQuery(sql).execute(getTuple(params)).coAwait()
 
     private fun getTuple(params: Array<out Any?>): Tuple {
         return Tuple.from(params.map {
-            if (it != null && it is Instant) {
-                OffsetDateTime.ofInstant(it, ZoneId.of("UTC"))
+            if (it != null) {
+                val actualValue = if (it::class.isValue) it::class.memberProperties.single().getter.call(it) else it
+                if (actualValue is Instant) OffsetDateTime.ofInstant(actualValue, ZoneId.of("UTC"))
+                else actualValue
             } else {
-                it
+                null
             }
         })
     }
 
-    suspend inline fun <reified T> selectOnePrimitive(sql: String, vararg params: Any?): T? {
-        return selectPrimitive<T>(sql, params = params).singleOrNull()
-    }
+    suspend inline fun <reified T> selectOnePrimitive(sql: String, vararg params: Any?): T? = selectPrimitive<T>(sql, params = params).singleOrNull()
 
-    suspend inline fun <reified T> selectOneExistingPrimitive(sql: String, vararg params: Any?): T {
-        return selectPrimitive<T>(sql, params = params).single()
-    }
+    suspend inline fun <reified T> selectOneExistingPrimitive(sql: String, vararg params: Any?): T = selectPrimitive<T>(sql, params = params).single()
+
 }
 

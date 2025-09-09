98f74beca97f521f3384e237ceb7f52d7749b5da
ssadat-guscheh-aimit <sascha.sadat-guscheh@scinteco.com>
2025-09-09 15:26:59 +0200

    update versions

--- buildSrc/src/main/kotlin/at.aimit.mariella.kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/at.aimit.mariella.kotlin-conventions.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 dependencies {
     // Align versions of all Kotlin components
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:2.2.0"))
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:2.2.10"))
 
     // Use the Kotlin JDK 8 standard library
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
--- buildSrc/src/main/kotlin/at/aimit/mariella/Versions.kt
+++ buildSrc/src/main/kotlin/at/aimit/mariella/Versions.kt
@@ -1,11 +1,11 @@
 package at.aimit.mariella
 
-const val vertxVersion = "5.0.2"
+const val vertxVersion = "5.0.4"
 const val logbackVersion = "1.5.18"
 const val postgresVersion = "42.7.7"
-const val flywayVersion = "11.11.0"
+const val flywayVersion = "11.12.0"
 const val h2Version = "2.3.232"
-const val hikariVersion = "7.0.1"
+const val hikariVersion = "7.0.2"
 const val scramClientVersion = "3.1"
 const val jakartaVersion = "3.2.0"
 const val slf4jVersion = "2.0.17"
